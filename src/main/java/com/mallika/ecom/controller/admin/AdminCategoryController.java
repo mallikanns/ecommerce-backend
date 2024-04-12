6fd0e08fc232f598d783f5e090b0f7babf68a987
mallika <mallika.nilns@gmail.com>
2024-04-13 00:19:16 +0700

    createCategory and getAllCategory

--- src/main/java/com/mallika/ecom/controller/admin/AdminCategoryController.java
+++ src/main/java/com/mallika/ecom/controller/admin/AdminCategoryController.java
@@ -3,25 +3,29 @@ package com.mallika.ecom.controller.admin;
 import com.mallika.ecom.dto.CategoryDto;
 import com.mallika.ecom.entity.Category;
 import com.mallika.ecom.services.admin.category.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminCategoryController {
-//    private final CategoryService categoryService;
-    @Autowired
-    private  CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @PostMapping("/category")
     public ResponseEntity<Category> createCategory(@RequestBody CategoryDto categoryDto) {
         Category category = categoryService.createCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Category>> getAllCategory() {
+        return ResponseEntity.ok(categoryService.getAllCategory());
     }
 
 }
--- src/main/java/com/mallika/ecom/services/admin/category/CategoryService.java
+++ src/main/java/com/mallika/ecom/services/admin/category/CategoryService.java
@@ -3,6 +3,10 @@ package com.mallika.ecom.services.admin.category;
 import com.mallika.ecom.dto.CategoryDto;
 import com.mallika.ecom.entity.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     Category createCategory(CategoryDto categoryDto);
+
+    List<Category> getAllCategory();
 }
