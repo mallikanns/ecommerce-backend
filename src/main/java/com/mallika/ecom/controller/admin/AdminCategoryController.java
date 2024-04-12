ebebc3c40d57a1ac6e5dad9ae021a42b1a3c49c3
mallika <mallika.nilns@gmail.com>
2024-04-12 19:15:10 +0700

    fix: category controller annotation

--- src/main/java/com/mallika/ecom/controller/admin/AdminCategoryController.java
+++ src/main/java/com/mallika/ecom/controller/admin/AdminCategoryController.java
@@ -6,6 +6,7 @@ import com.mallika.ecom.services.admin.category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AdminCategoryController {
     @Autowired
     private  CategoryService categoryService;
 
+    @PostMapping("/category")
     public ResponseEntity<Category> createCategory(@RequestBody CategoryDto categoryDto) {
         Category category = categoryService.createCategory(categoryDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(category);
