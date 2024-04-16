9edf7d5be16c37090a29b1afffbf4ada5145522d
mallika <mallika.nilns@gmail.com>
2024-04-16 17:29:14 +0700

    feat: searchOrderByTrackingId api

--- src/main/java/com/mallika/ecom/services/customer/cart/CartService.java
+++ src/main/java/com/mallika/ecom/services/customer/cart/CartService.java
@@ -6,6 +6,7 @@ import com.mallika.ecom.dto.PlaceOrderDto;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CartService {
     ResponseEntity<?> addProductToCart(AddProductInCartDto addProductInCartDto);
@@ -21,4 +22,6 @@ public interface CartService {
     OrderDto placeOrder(PlaceOrderDto placeOrderDto);
 
     List<OrderDto> getMyPlacedOrders(Long userId);
+
+    OrderDto searchOrderByTrackingId(UUID trackingId);
 }
--- src/main/java/com/mallika/ecom/services/customer/cart/CartServiceImpl.java
+++ src/main/java/com/mallika/ecom/services/customer/cart/CartServiceImpl.java
@@ -216,4 +216,12 @@ public class CartServiceImpl implements CartService {
         return orderRepository.findByUserIdAndOrderStatusIn(userId, List.of(OrderStatus.Placed, OrderStatus.Shipped,
                 OrderStatus.Delivered)).stream().map(Order::getOrderDto).collect(Collectors.toList());
     }
+
+    public OrderDto searchOrderByTrackingId(UUID trackingId) {
+        Optional<Order> optionalOrder = orderRepository.findByTrackingId(trackingId);
+        if (optionalOrder.isPresent()) {
+            return optionalOrder.get().getOrderDto();
+        }
+        return null;
+    }
 }
